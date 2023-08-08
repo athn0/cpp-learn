fb25539a0d1e760d0ccba7e242c8f97fb4d9ab7d
athn0 <133042076+athn0@users.noreply.github.com>
2023-08-07 21:59:58 -0500

    primeros pasos con punteros

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+using namespace std; 
 
-int main(int, char**){
-    std::cout << "Hello, from app-learn!\n";
+int main() {
+    cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	int a = 99;
+	int*b = &a;
+	cout << a<< endl;
+	cout << *b << endl;
+
+	return 0;
 }
