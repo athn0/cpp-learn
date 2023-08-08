bcfd7f7b5ba4f473b348f1ac039fdf12a005a4bd
athn0 <133042076+athn0@users.noreply.github.com>
2023-08-08 08:43:45 -0500

    funcion

--- main.cpp
+++ main.cpp
@@ -1,18 +1,11 @@
 #include <iostream>
-using namespace std; 
 
+void imprimir() {
+    std::cout << "Ejecutando la funcion imprimir()\n";
+}
 int main() {
-    // Modos de inicializar una var
-    // No afectan en el compilador, su funcion es visual
-    int a;       // no se recomienda, siempre inicializar
-    int b = 8;   // inicializacion por copia    
-    int c (6);   
-    int d { 9 }; // inicializacion por lista o uniforme 
     
-    // tres formas de inicializacion de lista
-    int x { 3 }; // inic directa
-    int y = {8}; // inic copia
-    int z {};    // inic valor  | inicializacion recomendada con valor vacio
-
+    imprimir();
+    
 	return 0;
 }
