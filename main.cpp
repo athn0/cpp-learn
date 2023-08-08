42acc1e85ed61803c5f17f85cfdec248f0de5782
athn0 <133042076+athn0@users.noreply.github.com>
2023-08-08 00:06:50 -0500

    modos de incializar variables

--- main.cpp
+++ main.cpp
@@ -2,11 +2,17 @@
 using namespace std; 
 
 int main() {
-    cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	int a = 99;
-	int*b = &a;
-	cout << a<< endl;
-	cout << *b << endl;
+    // Modos de inicializar una var
+    // No afectan en el compilador, su funcion es visual
+    int a;       // no se recomienda, siempre inicializar
+    int b = 8;   // inicializacion por copia    
+    int c (6);   
+    int d { 9 }; // inicializacion por lista o uniforme 
+    
+    // tres formas de inicializacion de lista
+    int x { 3 }; // inic directa
+    int y = {8}; // inic copia
+    int z {};    // inic valor  | inicializacion recomendada con valor vacio
 
 	return 0;
 }
