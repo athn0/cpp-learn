e0308a502fdba3fb8e1b16f9c94f4089ee2901ef
athn0 <133042076+athn0@users.noreply.github.com>
2023-08-14 23:28:22 -0500

    Funciones anticipadas

--- main.cpp
+++ main.cpp
@@ -1,4 +1,37 @@
 #include <iostream>
+#include <ostream>
+//* Funcion anticipadas: 
+/*
+ * permite declarara un funcion sim implementacion 
+ * para se llamado independientemente del orden
+ */
+/*
+
+void llamarB() {
+    llamarA(4, 4); // error por que la fucion no esta difinida todavia hasta que pase a la linea 12
+}
+
+void llamarA(int n1, int n2) {
+    llamarB();
+}
+
+*/
+
+// * Prototipos
+
+// void llammar_A(int, int );  //* Desaconsejable
+int llamar_A(int n1, int n2);
+
+void llamar_B() {
+    std::cout << "Funciones anticipadas\n";
+    std::cout << llamar_A(4, 4) << std::endl;
+}
+
+int llamar_A(int n1, int n2) {
+    return n1 + n2;
+}
+//* Funcion anticipadas - END
+
 
 void imprimir(int num) {
     std::cout << "Ejecutando la funcion imprimir()\n" << num <<"\n";
@@ -11,6 +44,7 @@ int devuelveNumerico(int num) {
 int main() {
     
     imprimir(devuelveNumerico(94));
-    
+    int numero { devuelveNumerico(2) };
+    llamar_B();
 	return 0;
 }
